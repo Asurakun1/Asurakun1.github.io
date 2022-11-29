895b977ec67a0850b25dfcaf067b1951d00c5557
Asurakun1 <udon3699@gmail.com>
2022-11-29 11:04:26 -0800

    made a clicker!

--- test.js
+++ test.js
@@ -1,10 +1,1 @@
-const api_url ="https://zenquotes.io/api/quotes/[your_key]";
-
-async function getapi(url)
-{
-  const response = await fetch(url);
-  let data = await response.json();
-  return data;
-}
-
-console.log(getapi(api_url).then(data => console.log(data)));
+document.querySelector(".mission").innerHTML = "Hello World!";
